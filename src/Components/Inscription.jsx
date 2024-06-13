1e2b9e7fefceaad22330d860a0cc078686f1397e
Perrine Deserville <mondierep@gmail.com>
2024-06-13 22:30:25 +0200

    Formulaire ok, le bouton enregistrer renvoi vers Characters pour l'instant, renvoi a modifier pour version finale

--- src/Components/Inscription.jsx
+++ src/Components/Inscription.jsx
@@ -1,51 +1,47 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Link } from "react-router-dom";
 import "./Inscription.css"
+
 
 
 function Inscription() {
     return(
-    <div className="login-box">
-        <form>
-            <div className="user-box">
-                <input type="text" name="Lastname" required/>
-                <label>Nom</label>
-            </div>
-            <div className="user-box">
-                <input type="text" name="Firstname" required/>
-                <label>Prénom</label>
-            </div>
-            {/* <div className="user-box">
-                <input type="date" name="Birthdate" required/>
-                <label>Date de<br />naissance</label>
-            </div> */}
-            <div className="user-box">
-                <input type="text" name="Species" required/>
-                <label>Ton espèce</label>
-            </div>
-            <div className="user-box">
-            <textarea
+        <div className="login-box">
+            <form>
+                <div className="user-box">
+                    <input type="text" name="Lastname" required/>
+                    <label>Nom</label>
+                </div>
+                <div className="user-box">
+                    <input type="text" name="Firstname" required/>
+                    <label>Prénom</label>
+                </div>
+                <div className="user-box">
+                    <input type="text" name="Species" required/>
+                    <label>Ton espèce</label>
+                </div>
+                <div className="user-box">
+                <textarea
                 rows="4" // Set the number of visible text lines
-                cols="27" // Set the width of the textarea (number of characters)
+                cols="32" // Set the width of the textarea (number of characters)
                 placeholder="Dévoilez-vous en quelques mots"
                 />
-            </div>
-            <div className="user-box">
-                <input type="text" name="Pseudo" required/>
-                <label>Pseudo</label>
-            </div>
-            <div className="user-box">
-                <input type="password" name="password" required/>
-                <label>Password</label>
-            </div>
-            <div className="user-box">
-            </div><center>
-                <a href="#">ENREGISTRER
-                <span></span>
-                </a></center>
-        </form> 
-    </div>
-    )
-};
+                </div>
+                <div className="user-box">
+                    <input type="text" name="Pseudo" required/>
+                    <label>Pseudo</label>
+                </div>
+                <div className="user-box">
+                    <input type="password" name="password" required/>
+                    <label>Password</label>
+                </div>
+                <div className="user-box">
+                </div><center>
+                    <Link className="a" to="/Characters">ENREGISTRER
+                    <span></span>
+                    </Link></center>
+            </form> 
+        </div>
+)};
 
 
 export default Inscription;

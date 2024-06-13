e9a3b8a9c637ac2b8c97b23b055c43ac3f7e694e
NICOLASR44 <158582207+NICOLASR44@users.noreply.github.com>
2024-06-14 00:46:55 +0200

    final version ?

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from "./App.jsx";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Characters from "./Components/Characters.jsx";
+import Page4 from "./Components/Page4.jsx";
 
 const router = createBrowserRouter([
   {
@@ -13,6 +14,10 @@ const router = createBrowserRouter([
   {
     path: "/Characters",
     element: <Characters />,
+  },
+  {
+    path: "/Page4",
+    element: <Page4 />,
   },
 ]);
 
